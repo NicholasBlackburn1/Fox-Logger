eae7efd31ac799b4455cd0b2b8f1e50f916f2630
Nicholas Blackburn <NickyBlackburn@MRS-PC.corp.mrsmmc.com>
2022-05-13 09:22:24 -0400

    foxlogger needs testing to see if it works

--- Fox Logger/Properties/AssemblyInfo.cs
+++ Fox Logger/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AvatarLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nicky Blackburn")]
 [assembly: AssemblyProduct("AvatarLogger")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
